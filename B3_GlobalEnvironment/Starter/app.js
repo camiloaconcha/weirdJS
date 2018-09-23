9a883fcfcc9ea08a85c73cb6568e10a539221ab7
Camilo Concha <camiloconcha@Camilos-MacBook-Pro.local>
2018-09-23 12:28:58 -0500

    Going to Synchronous

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,8 +1,23 @@
 
+/* First hoisting example*/
+
+function b() {
+    console.log('Called b');
+}
 b();
+
+console.log(a);
+var a = "Hello World"; 
 console.log(a);
 
-var a = "Hello World"; 
-function b() {
- console.log('called b');
-}
+
+
+
+/*var a = "Hello World";
+console.log(a);
+
+if (a === undefined) {
+    console.log('a is undefined');
+}else{
+    console.log('a is defined!');
+}*/
