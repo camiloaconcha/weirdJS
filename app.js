c8b167a8d4c55846be66179cca0c9f18e0a6c3a1
Camilo Concha <camiloconcha@Camilos-MacBook-Pro.local>
2018-10-01 22:10:50 -0500

    Equality and operators

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log(1<2<3);
+console.log(3<2<1);
+//becomes 3<2 = false;
+console.log(false<1);
+//and also false becomes Number(false) = 0;
+console.log(0<1);
+//It makes has sense that the result is true
