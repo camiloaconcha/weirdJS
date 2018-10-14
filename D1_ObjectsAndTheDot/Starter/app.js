a60d906530e4bf5fd420b122e29ee2c38c9a5292
Camilo Concha <camiloconcha@Camilos-MacBook-Pro.local>
2018-10-13 23:10:01 -0500

    Objects And The Dot

---
+++ D1_ObjectsAndTheDot/Starter/app.js
@@ -0,0 +1,24 @@
+var person = new Object();
+
+person["firstname"] = "Tony";
+person["lastname"] = "Alicea";
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "111 Main St";
+person.address.city = "Bogotá";
+person.address.state = "Cundinamarca";
+
+console.log(person.address.street);
+console.log(person.address.city);
+
+console.log(person["address"]["state"]);
+
+
